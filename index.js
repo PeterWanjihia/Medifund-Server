04c18a303aedfb11c657a1035fc2848147346172
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2023-03-16 23:56:57 +0300

    changes commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors("*"));
+app.use(cors({ origin: "*" }));
 
 app.post("/payment", cors(), async (req, res) => {
   let { amount, id } = req.body;
