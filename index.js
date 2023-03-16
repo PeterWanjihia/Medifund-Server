841ec5c3ce8e2e6360f1339057e71dddd6d4a325
PeterWanjihia <peter.wanjihia@student.moringaschool.com>
2023-03-17 00:16:12 +0300

    Added: changes to server

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 
 const stripe = require("stripe")(process.env.SECRET_KEY);
-const cors = require("cors");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,11 +12,13 @@ app.use(cors());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "OPTIONS, POST");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
+
 
 app.post("/payment", cors(), async (req, res) => {
   let { amount, id } = req.body;
